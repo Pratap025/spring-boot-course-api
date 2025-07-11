89897c467cf15667bcaa73e4958cc42c92a96a47
Uday Pratap <bagheludaypratap025@gmail.com>
2025-07-11 15:38:17 +0530

    "Api for get"

--- src/main/java/com/CourseManagementApplication/controller/CourseController.java
+++ src/main/java/com/CourseManagementApplication/controller/CourseController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,5 +27,10 @@ public class CourseController {
 	        return new ResponseEntity<>(courImpl.create(course), HttpStatus.CREATED);
 	    }
 	
-	
+	  @GetMapping("/get")
+	public String getAll() {
+		return "we have tested";
+		
+	}
+	  
 }
